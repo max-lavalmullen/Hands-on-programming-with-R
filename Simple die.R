e0f1e837fc50e5ae0c1e20a96573892c3ab59138
max-lavalmullen <167341594+max-lavalmullen@users.noreply.github.com>
2024-06-04 23:58:49 -0400

    Update Simple die.R

--- Simple die.R
+++ Simple die.R
@@ -3,3 +3,25 @@ roll <- function() {
     die <- 1:6
     sample(die, 2, TRUE)
 }
+
+# same as roll(), but sums result
+roll2 <- function() { 
+  die <- 1:6
+  result <- sample(die, 2, TRUE)
+  sum(result)
+}
+
+# replicating die rolls and plotting them
+replicate(20, roll2())
+
+# fun with plotz
+library("ggplot2")
+x <- c(-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1)
+y <- x^3
+qplot(x, y)
+
+z <- c(1, 2, 2, 2, 3, 3)
+qplot(z, binwidth = 1)
+
+x3 <- c(0, 1, 1, 2, 2, 2, 3, 3, 4)
+qplot(x3, binwidth = 1)
