be4f7479bfded07f7dd6d691400f60b0bbf3032e
max-lavalmullen <167341594+max-lavalmullen@users.noreply.github.com>
2024-06-10 19:47:01 -0400

    Update Weighted Die.R

--- Weighted Die.R
+++ Weighted Die.R
@@ -1,1 +1,14 @@
+# rolls two dice, which are weighted according to
+# a user supplied weight vector.
+# the resulting values of the dice are then summed.
+roll <- function(weight) {
+  die <- 1:6
+  dice <- sample(die, size = 2, replace = TRUE,
+                 prob = weight)
+  sum(dice)
+}
 
+library(ggplot2)
+weighted <- c(1/8, 1/8, 1/8, 1/8, 1/8, 3/8)
+rolls <- replicate(100000, roll(weighted))
+qplot(rolls, binwidth = 1)
