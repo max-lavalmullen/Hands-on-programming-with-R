33e93c1675c48d862deee15de460a20707890531
max-lavalmullen <167341594+max-lavalmullen@users.noreply.github.com>
2024-06-14 15:55:35 -0400

    Update Deck of Cards.R

--- Deck of Cards.R
+++ Deck of Cards.R
@@ -1,1 +1,12 @@
+hand1 <- c("ace", "king", "queen", "jack", "ten", "spades", "spades", 
+           "spades", "spades", "spades")
 
+matrix(hand1, nrow = 5)
+matrix(hand1, ncol = 2)
+dim(hand1) <- c(5, 2)
+
+hand2 <- c("ace", "spades", "king", "spades", "queen", "spades", "jack", 
+           "spades", "ten", "spades")
+
+matrix(hand2, nrow = 5, byrow = TRUE)
+matrix(hand2, ncol = 2, byrow = TRUE)
